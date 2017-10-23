07821ea4136f04b3866fee924e3ac25c35e88531
Regis Lessa <regis23.lf@gmail.com>
2017-10-23 20:35:32 -0200

    HTML +Css
    aprendendo html+css+design responsivo (AlgaWorks)

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var botaoAlternador = document.querySelector('.js-botao-chaveador');
+
+botaoAlternador.onclick = function() {
+
+	var menu = document.querySelector('.js-menu');
+	menu.classList.toggle('menu--exibindo');
+}
